52a5eb197aa58ead11754fa4d0e7a0204f4f5301
RobD6 <robdodd6@googlemail.com>
2023-12-02 20:36:37 +0000

    Add Day3 and template files

---
+++ Day3/Day3.cs
@@ -0,0 +1,18 @@
+using System.Text.RegularExpressions;
+
+public static class Day3
+{
+	public static int PartOne()
+	{
+		var lines = File.ReadAllLines("Day3.txt");
+
+		return lines.Length;
+	}
+	
+	public static int PartTwo()
+	{
+		var lines = File.ReadAllLines("Day3.txt");
+
+		return lines.Length;
+	}
+}
---
+++ DayX.cs
@@ -0,0 +1,18 @@
+using System.Text.RegularExpressions;
+
+public static class DayX
+{
+	public static int PartOne()
+	{
+		var lines = File.ReadAllLines("DayX.txt");
+
+		return lines.Length;
+	}
+	
+	public static int PartTwo()
+	{
+		var lines = File.ReadAllLines("DayX.txt");
+
+		return lines.Length;
+	}
+}
